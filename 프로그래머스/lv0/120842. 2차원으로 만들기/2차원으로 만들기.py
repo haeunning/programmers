b004c162cda5d17e08e224c05075c47f19e7d776
Haeun Kim <haeunning@yonsei.ac.kr>
2023-04-25 10:53:26 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,4 +1,5 @@
-import numpy as np
 def solution(num_list, n):
-    answer = np.array(num_list).reshape(-1,n)
-    return answer.tolist()
+    answer=[]
+    for i in range(0, len(num_list),n):
+        answer.append(num_list[i:i+n])
+    return answer
