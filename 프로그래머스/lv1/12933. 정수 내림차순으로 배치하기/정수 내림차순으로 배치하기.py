fa3b157c9a613cf8ca0339ee6d30040f64feae4a
Haeun Kim <haeunning@yonsei.ac.kr>
2023-05-18 17:11:59 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    a = []
+    n = str(n)
+    for i in n:
+        a.append(int(i))
+    a = sorted(a, reverse=True)
+    b=''
+    for i in a:
+        b += str(i)
+    return int(b)
