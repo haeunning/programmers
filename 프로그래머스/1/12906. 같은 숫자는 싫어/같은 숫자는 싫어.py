7562f89643155341fef30f201ab203d2d723ef16
Haeun Kim <haeunning@yonsei.ac.kr>
2023-10-22 00:08:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 130.96 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[0])
+        else:
+            if arr[i-1] != arr[i]:
+                answer.append(arr[i])
+    return answer
