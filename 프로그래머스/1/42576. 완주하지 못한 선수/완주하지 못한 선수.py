b8fc47915870242506a0c7cb8689fc3688bb1d63
Haeun Kim <haeunning@yonsei.ac.kr>
2024-04-09 18:36:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.59 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,15 @@
-from collections import Counter
+# from collections import Counter
+# def solution(participant, completion):
+#     count_p = Counter(participant)
+#     count_c = Counter(completion)
+#     for i in count_p:
+#         if count_p[i] > count_c[i]:
+#             return i
+
 def solution(participant, completion):
-    count_p = Counter(participant)
-    count_c = Counter(completion)
-    for i in count_p:
-        if count_p[i] > count_c[i]:
-            return i
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
