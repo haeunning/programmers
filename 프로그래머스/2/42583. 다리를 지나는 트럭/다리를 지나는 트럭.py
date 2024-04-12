b4187a4e3565683d17fbcdbd6946f44461d9c72c
Haeun Kim <haeunning@yonsei.ac.kr>
2024-04-12 21:59:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    time=0
+    bridge = deque([0]*bridge_length)
+    truck_weights = deque(truck_weights)
+    current_weight=0
+    while len(truck_weights)>0:
+        time += 1
+        current_weight -= bridge.popleft()
+        if current_weight+truck_weights[0]<=weight:
+            current_weight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+        else:
+            bridge.append(0)
+    time += bridge_length
+    return time
